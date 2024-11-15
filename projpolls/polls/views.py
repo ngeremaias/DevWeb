7f66d1245af3662b5d19e61b9c6f43018a0f93ee
Jeremias Bezerra Lucas <88001432+ngeremaias@users.noreply.github.com>
2024-11-15 21:09:11 +0000

    criação de templates

--- projpolls/polls/views.py
+++ projpolls/polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Bem-vindo(a) à aplicação Enquete</h1")
+    #- indica que ele vai colocar em ordem decrescente
+    lista_questoes=Question.objects.order_by('-pub_date')[:5]
+    context={'lista_questoes':lista_questoes}
+    return render(request,'index.html',context)
+def details(request,question_id):
+    response=f'Você está visualizando a questão{question_id}'
+    return HttpResponse(response)
+def results(request,question_id):
+    response=f'Resultado da questão {question_id}'
+    return HttpResponse(response)
+def vote(request,question_id):
+    response=f'Votação para a questão {question_id}'
+    return HttpResponse(response)
